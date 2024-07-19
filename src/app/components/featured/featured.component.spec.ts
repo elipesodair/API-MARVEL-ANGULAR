5f14949998074737d32480f9384a9e6dbcc392d1
Elipes <elipes.sindon@lavembebe.com.br>
2024-07-19 19:06:09 -0300

    atualização-css

--- src/app/components/featured/featured.component.spec.ts
+++ src/app/components/featured/featured.component.spec.ts
@@ -1,17 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FeaturedComponent } from './featured.component';
 
 describe('FeaturedComponent', () => {
   let component: FeaturedComponent;
   let fixture: ComponentFixture<FeaturedComponent>;
 
+
+  beforeAll(() => {
+
+  });
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FeaturedComponent ]
-    })
-    .compileComponents();
+      declarations: [FeaturedComponent],
+    }).compileComponents();
   });
+
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FeaturedComponent);
@@ -19,7 +23,19 @@ describe('FeaturedComponent', () => {
     fixture.detectChanges();
   });
 
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('should have a defined property', () => {
+    expect(component.someProperty).toBeDefined();
+  });
+
+  
+  it('should call a method and return expected value', () => {
+    const returnValue = component.someMethod();
+    expect(returnValue).toBe(expectedValue);
   });
 });
